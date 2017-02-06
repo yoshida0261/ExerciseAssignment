197f208adb5f524eb751a5bb1b1a62d66c284ad6
yoshida0261 <yoshida261@gmail.com>
2017-02-07 01:27:59 +0900

    Initial commit

---
+++ src/com/company/stah/Main.java
@@ -0,0 +1,9 @@
+package com.company.stah;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/com/company/stah/Schedule.java
@@ -0,0 +1,7 @@
+package com.company.stah;
+
+/**
+ * Created by takeshi on 17/02/07.
+ */
+public class Schedule {
+}
