e24356f998adc19cee25eaa5660f9a9b29aa7e7a
yoshida0261 <yoshida261@gmail.com>
2017-02-09 01:59:19 +0900

    OptionParserでUsageを表示するようになったので不要なクラスを削除

--- src/com/company/stah/Search.java
+++ src/com/company/stah/Search.java
@@ -1,6 +1,13 @@
 package com.company.stah;
 
+import java.util.ArrayList;
+
 public class Search {
 
+    public ArrayList<String> binarySearch()
+    {
+        //TODO
+        return null;
+    }
 
 }
